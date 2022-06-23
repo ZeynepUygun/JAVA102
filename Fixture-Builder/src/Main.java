bebea3eb78584adc09f45083dd4b6067623e30c1
Zeynep UYGUN <zynpzynp.uygun@gmail.com>
2022-06-23 04:09:07 +0300

    Fixture Builder completed.

---
+++ Fixture-Builder/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Teams teams = new Teams();
+
+        teams.teamList.add("Galatasaray");
+        teams.teamList.add("FenerBahce");
+        teams.teamList.add("Besiktas");
+        teams.teamList.add("Bursaspor");
+        teams.teamList.add("Hatayspor");
+        teams.teamList.add("Trabzonspor");
+        teams.teamList.add("Boluspor");
+
+
+        teams.ligFixturePrint();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
